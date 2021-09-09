bb4cd4b362338094758895088e75367fd64484d3
Erick Eduardo Lopez Nava <wall.e50@hotmail.com>
2021-09-09 22:16:44 +0000

    test-lab

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+	var name = ""
+	if len(os.Args) < 2 {
+		fmt.Println("Not enoguh data")
+		return
+	} else if len(os.Args) == 2 {
+		name = os.Args[1]
+	} else if len(os.Args) > 2 {
+		name = ""
+		for i := 1; i < len(os.Args); i++ {
+			name += os.Args[i] + " "
+		}
+	}
+	fmt.Printf("Welcome %sto the jungle\n", name)
+
 }
