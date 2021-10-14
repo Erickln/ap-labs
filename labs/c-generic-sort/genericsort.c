095d6d988ddaccc824b0cc02505665e9cc3003b4
Erick Eduardo Lopez Nava <wall.e50@hotmail.com>
2021-10-14 03:14:40 +0000

    Done

--- labs/c-generic-sort/genericsort.c
+++ labs/c-generic-sort/genericsort.c
@@ -1,12 +1,136 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-void quicksort(void *lineptr[], int left, int right,
-	   int (*comp)(void *, void *));
+#define MLX 5000
+#define MLEN 1000
+#define ALLOC 100000
+static char allocbuf[ALLOC];
+static char *allocp = allocbuf;
+char *lptr[MLX];
+int lines;
 
-void mergesort(void *lineptr[], int left, int right,
-	   int (*comp)(void *, void *));
+char *alloc(int n){
+	if(allocbuf + ALLOC - allocp >= n ){
+		allocp += n;
+		return allocp - n;
+	}else{
+		return 0;
+	}
+}
 
-int main(int argc, char **argv)
-{
-    return 0;
+void writelines(char *lptr[], int nl, char *name){
+	FILE *fp;
+	char *nn = malloc(sizeof(name) + 9);
+	char *prefix = "sorted_";
+	strcpy(nn, prefix);
+	strcat(nn, name);
+	printf("... Results file can be found at ./%s \n", nn);
+	fp = fopen(nn, "w");
+	if(fp == NULL){
+		exit(-1);
+	}
+	for(int i = 1; i < nl; i++){
+		fprintf(fp,"%s", lptr[i-1]);
+	}
+	fclose(fp);
+}
+
+int readlines(char *lptr[], int mls, char *file){
+	char *p;
+	int lines = 0;
+	FILE *fp = fopen(file, "r");
+	if(fp == NULL){
+		printf("Error, no se pudo abrir el archivo\n");
+		return 1;
+	}
+	char *line = NULL;
+	size_t len = 0;
+	while(getline(&line, &len, fp) != -1 )
+		if(lines >= mls || (p = alloc(len)) == NULL){
+			return -1;
+		}else{
+			line[len-1] = '\0';
+			strcpy(p, line);
+			lptr[lines++] = p;
+		}
+
+	fclose(fp);
+	free(line);
+	return lines;
+}
+
+int numcmp(char *s1, char *s2){
+	double v1,v2;
+	v1 = atof(s1);
+	v2 = atof(s2);
+	if(v1 < v2)
+		return -1;
+	else if(v1 > v2)
+		return 1;
+	else
+		return 0;
+}
+
+void quicksort(void *lptr[], int left, int right, int (*comp)(void *, void *));
+
+void mergesort(void *choicename[], int left, int right, int (*comp)(void *, void *));
+
+int main(int argc, char **argv){
+	int choice;
+	int nl;
+	int aux = 0;
+	char *filen, *choicename;
+	int arg;
+	if(argc > 1 && strcmp(argv[1], "-n") == 0){ //Comparacion numerica
+		choice = 3;
+		arg = 2;
+  	aux = 1;
+	}else{
+    choice = 2;
+		arg = 1;
+	}
+	filen = argv[arg];
+	choicename = argv[choice];
+	char * quick = strstr(choicename, "quick");
+	char * merge = strstr(choicename, "merge");
+	if(quick){
+		printf("... sorting %s file with quicksort\n", filen);
+		if((nl = readlines(lptr, MLX, argv[arg])) >= 0){
+			if(aux){
+				quicksort((void**) lptr, 0, nl-1, (int (*) (void*, void*)) (numcmp));
+			}else{
+				quicksort((void**) lptr, 0, nl-1, (int (*) (void*, void*)) (strcmp));
+			}
+			if(aux){
+				mergesort((void**) lptr, 0, nl-1, (int (*) (void*, void*)) (numcmp));
+			}else{
+				mergesort((void**) lptr, 0, nl-1, (int (*) (void*, void*)) (strcmp));
+			}
+			writelines(lptr, nl, filen);
+			return 0;
+		}else{
+			printf("Input too big or to sort \n");
+			return 1;
+		}
+
+	}else if (merge){
+		printf("... sorting %s file with mergesort\n", filen);
+		if((nl = readlines(lptr, MLX, argv[arg])) >= 0){
+			if(aux){
+				mergesort((void**) lptr, 0, nl, (int (*) (void*,void*)) (numcmp));
+			}else{
+				mergesort((void**) lptr, 0, nl, (int (*) (void*,void*)) (strcmp));
+			}
+			writelines(lptr, nl, filen);
+			return 0;
+		}else{
+			printf("Input too big or to sort \n");
+			return 1;
+		}
+	}else{
+		printf("ERROR, please check the arguments used. \n");
+		return -1;
+	}
+	return 0;
 }
--- labs/c-generic-sort/mergesort.c
+++ labs/c-generic-sort/mergesort.c
@@ -1,4 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-void mergesort(void *lineptr[], int left, int right,
-	       int (*comp)(void *, void *)) {
+
+
+void cparray(char *lineptr[], int l, int r, char *aux[]){
+
+	for(int k = l; k < r; k++){
+		aux[k] = lineptr[k];
+	}
+}
+
+void split(char *aux[], int l, int r, char *lptr,int (*comp)(void *, void *)){
+	if(r - l <= 1){
+		return;
+  }
+	int m = (r + l) / 2;
+	split(lptr, l, m, aux, comp);
+	split(lptr, m, r, aux, comp);
+	merge(aux, l, m, r, lptr, comp);
+}
+
+void merge(char *lp[], int l, int m, int r, char *aux[], int (*comp)(void *, void *)){
+	int i = l, j = m;
+
+	for(int k = l; k < r; k++){
+		if(i < m && (j >= r || (*comp)(lp[i], lp[j]) <= 0)){
+			aux[k] = lp[i++];
+		}else{
+			aux[k] = lp[j++];
+		}
+	}
+}
+
+void mergesort(void *lineptr[], int left, int right, int (*comp)(void *, void *)) {
+	char *auxlineptr[10000];
+	cparray(lineptr, left, right, auxlineptr);
+	split(auxlineptr, left, right,lineptr, comp);
+
 }
